c4c83a383d342eaa7501bde2b4152c2c7bb472f8
nathaliavenerayan <47015176+nathaliavenerayan@users.noreply.github.com>
2020-05-25 15:35:52 -0400

    Created a google map

---
+++ my-map/coords.js
@@ -0,0 +1,53 @@
+var random = 
+
+var marker = [
+    {
+        "coords": [40.7784986,-73.8780262],
+        "amount": random,
+        "name": "Central Park"
+    },
+    {
+        "coords": [43.695552, -79.540864],
+        "amount": random,
+        "name": "Alex Marchetti Park"
+    },
+    {
+        "coords": [43.695185, -79.533956],
+        "amount": 10,
+        "name": "Valleyfield Park"
+    },
+    {
+        "coords": [43.696504, -79.512938],
+        "amount": 25,
+        "name": "Raymore Park"
+    },
+    {
+        "coords": [43.671785, -79.514558],
+        "amount": random,
+        "name": "James Gardens"
+    },
+    {
+        "coords": [43.725772, -79.553783],
+        "amount": random,
+        "name": "West Humber Parkland"
+    },
+    {
+        "coords": [43.731602, -79.548364],
+        "amount": random,
+        "name": "Summerlea Park"
+    },
+    {
+        "coords": [43.711938, -79.549008],
+        "amount": random,
+        "name": "Pine Point Park"
+    },
+    {
+        "coords": [],
+        "amount": ,
+        "name": ""
+    },
+    {
+        "coords": [],
+        "amount": ,
+        "name": ""
+    },
