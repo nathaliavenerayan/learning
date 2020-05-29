8aab6285f570111ae926656911281cfa15634d14
nathaliavenerayan <47015176+nathaliavenerayan@users.noreply.github.com>
2020-05-29 15:19:07 -0400

    Added folder

---
+++ react/learn-react-yt/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+ 
+export class MapContainer extends React.Component {
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+ 
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyDQYVNALtx5wUORTW5uizV6_Jw_pYcEUeM")
+})(MapContainer)
